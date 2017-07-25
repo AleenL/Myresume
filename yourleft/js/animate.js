878224160af77679b16f3acd26d594a4395dcf93
李子豪 <lizh@xiaojs.cn>
2017-07-25 17:16:00 +0800

    更新了样式

--- yourleft/js/animate.js
+++ yourleft/js/animate.js
@@ -9,11 +9,7 @@ window.onload = function() {
 		loadPage2 = $('.loadPage')
 
 	loadPage2.fadeOut()
-	$('body').css({
-		width:'auto',
-		height:'auto',
-		overflow:'auto'
-	})
+	$('body').removeClass('over')
 	$('#yourLeft').fullpage({
 		scrollingSpeed: 800,
 		anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8', 'page9'],
